5845e114b868daf60daa9970a4f0c78c62a63ac3
karbproton <karbing@protonmail.com>
2024-02-20 05:02:01 +0100

    iac test file

---
+++ iac_files/ec2_aws_access_key.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "web_host" {
+  # ec2 have plain text secrets in user data
+  ami           = "${var.ami}"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [
+  "${aws_security_group.web-node.id}"]
+  subnet_id = "${aws_subnet.web_subnet.id}"
+  user_data = <<EOF
+#! /bin/bash
+export AWS_ACCESS_KEY_ID=AKIAQWERTYUIOPQQMAAA
+export AWS_SECRET_ACCESS_KEY=wJalQWERTYUIOPQWERTYUIOPQWERTYUIOPQWAKEY
+EOF
+  tags = merge({
+    Name = "${local.resource_prefix.value}-ec2"
+    }, {
+    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+  })
+}
